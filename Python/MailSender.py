8aa226cbfeef4159a5679bea8c475983bc896451
janbor2YNG <jansch5@schuldorf.de>
2025-11-14 16:50:26 +0100

    Initial commit

--- Python/MailSender.py
+++ Python/MailSender.py
@@ -3,6 +3,9 @@ import os
 import random
 import smtplib
 import yaml
+import schedule
+import time
+from datetime import datetime, timedelta
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -14,10 +17,48 @@ SMTP_PORT = 587
 SENDER_EMAIL = "teacherfisher.innen@gmail.com"
 SENDER_PASSWORD = "czqc rfzf qijm vvnf"
 
+START = config_cpgn["send"]["timeframe"]["start"]       # "01.01.2026"
+END = config_cpgn["send"]["timeframe"]["end"]           # "01.02.2026"
+MAIL_COUNT = config_cpgn["send"]["total_emails"]  # z.B. 100
+start_time = "16:50"
+start_time_zum_rechnen = 16
 # Betreff
 subject = "PLATZHALTER"
 mail_body_randomnes = config_cpgn["mail_body_randomnes"]
 
+def verteile_mails(startdatum, enddatum, mail_count):
+    start = datetime.strptime(startdatum, "%d.%m.%Y")
+    end = datetime.strptime(enddatum, "%d.%m.%Y")
+
+    tage = (end - start).days + 1
+
+    mails_pro_tag = [mail_count // tage] * tage
+    rest = mail_count % tage
+
+    for i in random.sample(range(tage), rest):
+        mails_pro_tag[i] += 1
+
+    plan = {}
+    for offset, mails in enumerate(mails_pro_tag):
+        tag = (start + timedelta(days=offset)).strftime("%d.%m.%Y")
+        plan[tag] = mails
+
+    return plan
+
+VERTEILPLAN = verteile_mails(START, END, MAIL_COUNT)
+def daily_job():
+    heute = datetime.now().strftime("%d.%m.%Y")
+
+    if heute not in VERTEILPLAN:
+        print("Heute müssen keine Mails versendet werden.")
+        return
+
+    anzahl = VERTEILPLAN[heute]
+    geteilte_zeit = (24-start_time_zum_rechnen) // (anzahl-1)*3600
+    waiting_time = random.randrange(0,geteilte_zeit)
+    print(f"waiting_time{waiting_time}.sekunden")
+    print(f"Heute ({heute}) werden {anzahl} Mails verschickt.")
+    send_mail(anzahl, waiting_time)
 
 chosen_mail_body = "placeholder"
 def declareMailBody(dateiname):
@@ -27,43 +68,47 @@ def declareMailBody(dateiname):
     with open(fr"..\Server\Mails\{dateiname}", "r", encoding="utf-8") as file:
         mail_body = file.read()
     return mail_body
+def send_mail(anzahl, waiting_time):
+    for i in range(anzahl):
+        with open(r"..\Server\Datenbanken\current_user_list.csv", newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
 
-if mail_body_randomnes:
-    # Alle Dateien im Ordner auflisten (nur Dateien, keine Unterordner)
-    list_mail_bodys = [f for f in os.listdir(r"..\Server\Mails") if os.path.isfile(os.path.join(r"..\Server\Mails", f))]
-    # Zufällige Datei auswählen
-    chosen_mail_body = random.choice(list_mail_bodys)
-    print(f"Chosen mail: {chosen_mail_body}")
-    mail_body = declareMailBody(chosen_mail_body)
-else:
-    chosen_mail_body = config_cpgn["body_template"]
-    mail_body = declareMailBody(chosen_mail_body)
-    print(f"Chosen mail: {chosen_mail_body}")
-# CSV einlesen
-with open(r"..\Server\Datenbanken\current_user_list.csv", newline="", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
+            # SMTP-Verbindung einmal öffnen
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+                server.starttls()
+                server.login(SENDER_EMAIL, SENDER_PASSWORD)
 
-    # SMTP-Verbindung einmal öffnen
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.starttls()
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+                for row in reader:
+                    user_mail = row["email"].strip()
+                    name = row["name"].strip()
+                    lastname = row["lastname"].strip()
 
-        for row in reader:
-            user_mail = row["email"].strip()
-            name = row["name"].strip()
-            lastname = row["lastname"].strip()
+                    # Mail zusammenbauen
+                    mime_message = MIMEMultipart()
+                    mime_message["From"] = SENDER_EMAIL
+                    mime_message["To"] = user_mail
+                    mime_message["Subject"] = subject
 
-            # Mail zusammenbauen
-            mime_message = MIMEMultipart()
-            mime_message["From"] = SENDER_EMAIL
-            mime_message["To"] = user_mail
-            mime_message["Subject"] = subject
+                    # Individueller Text
+                    body = mail_body.format(name=name, lastname=lastname, user_mail=user_mail)
+                    mime_message.attach(MIMEText(body, "html"))
 
-            # Individueller Text
-            body = mail_body.format(name=name, lastname=lastname, user_mail=user_mail)
-            mime_message.attach(MIMEText(body, "html"))
+                    # Mail senden
+                    server.sendmail(SENDER_EMAIL, user_mail, mime_message.as_string())
+                    print(f"Sent to: {user_mail}")
+        time.sleep(waiting_time)
 
 
-            # Mail senden
-            server.sendmail(SENDER_EMAIL, user_mail, mime_message.as_string())
-            print(f"Sent to: {user_mail}")
+
+list_mail_bodys = config_cpgn["body_template"]
+
+schedule.every().day.at(start_time).do(daily_job)
+
+# Zufällige Datei auswählen
+chosen_mail_body = str(random.choice(list_mail_bodys) + ".html")
+print(f"Chosen mail: {chosen_mail_body}")
+mail_body = declareMailBody(chosen_mail_body)
+
+
+while True:
+    schedule.run_pending()
