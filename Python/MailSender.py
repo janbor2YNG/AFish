610a686f63b345dbe952ae21746029ed37e74875
janbor2YNG <jansch5@schuldorf.de>
2026-02-13 14:37:41 +0100

    Initial commit

--- Python/MailSender.py
+++ Python/MailSender.py
@@ -11,6 +11,7 @@ from email.mime.multipart import MIMEMultipart
 
 with open("..\Server\Datenbanken\cpgn1.yaml", "r", encoding="utf-8") as file:
     config_cpgn = yaml.safe_load(file)
+id = 0
 # Absender-Infos
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
@@ -20,8 +21,8 @@ SENDER_PASSWORD = "czqc rfzf qijm vvnf"
 START = config_cpgn["send"]["timeframe"]["start"]       # "01.01.2026"
 END = config_cpgn["send"]["timeframe"]["end"]           # "01.02.2026"
 MAIL_COUNT = config_cpgn["send"]["total_emails"]  # z.B. 100
-start_time = "07:59"
-start_time_zum_rechnen = 16
+start_time = "15:26"
+start_time_zum_rechnen = 14
 # Betreff
 subject = "PLATZHALTER"
 mail_body_randomnes = config_cpgn["mail_body_randomnes"]
@@ -48,17 +49,26 @@ def verteile_mails(startdatum, enddatum, mail_count):
 VERTEILPLAN = verteile_mails(START, END, MAIL_COUNT)
 def daily_job():
     heute = datetime.now().strftime("%d.%m.%Y")
+    global VERTEILPLAN
 
     if heute not in VERTEILPLAN:
         print("Heute müssen keine Mails versendet werden.")
         return
 
     anzahl = VERTEILPLAN[heute]
-    geteilte_zeit = (24-start_time_zum_rechnen) // (anzahl-1)*3600
-    waiting_time = random.randrange(0,geteilte_zeit)
+    if anzahl <= 1:
+        waiting_time = 0
+    else:
+        gesamtzeit = (24-start_time_zum_rechnen) // (anzahl-1)*3600
+        geteilte_zeit = gesamtzeit // (anzahl - 1)
+        if geteilte_zeit <= 0:
+            waiting_time = 0
+        else:
+            waiting_time = random.randrange(0, geteilte_zeit)
     print(f"waiting_time{waiting_time}.sekunden")
     print(f"Heute ({heute}) werden {anzahl} Mails verschickt.")
     send_mail(anzahl, waiting_time)
+
 
 chosen_mail_body = "placeholder"
 def declareMailBody(dateiname):
@@ -69,7 +79,11 @@ def declareMailBody(dateiname):
         mail_body = file.read()
     return mail_body
 def send_mail(anzahl, waiting_time):
+
+
     for i in range(anzahl):
+        global id
+        id += 1
         with open(r"..\Server\Datenbanken\current_user_list.csv", newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
 
@@ -90,13 +104,14 @@ def send_mail(anzahl, waiting_time):
                     mime_message["Subject"] = subject
 
                     # Individueller Text
-                    body = mail_body.format(name=name, lastname=lastname, user_mail=user_mail)
+                    body = mail_body.format(id=id, name=name, lastname=lastname, user_mail=user_mail)
                     mime_message.attach(MIMEText(body, "html"))
 
                     # Mail senden
                     server.sendmail(SENDER_EMAIL, user_mail, mime_message.as_string())
                     print(f"Sent to: {user_mail}")
         time.sleep(waiting_time)
+    return id
 
 
 
@@ -108,7 +123,6 @@ schedule.every().day.at(start_time).do(daily_job)
 chosen_mail_body = str(random.choice(list_mail_bodys) + ".html")
 print(f"Chosen mail: {chosen_mail_body}")
 mail_body = declareMailBody(chosen_mail_body)
-
 
 while True:
     schedule.run_pending()
